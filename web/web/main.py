fd3d4be5e2733df3a3113c34da7ba6da03337bd7
Winterfellwen <38210291+Winterfellwen@users.noreply.github.com>
2023-07-03 15:02:22 +0800

    Update main.py

--- web/web/main.py
+++ web/web/main.py
@@ -1,1 +1,19 @@
+from flask import Flask, render_template, request
+import subprocess
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_vm')
+def get_vm():
+    result = subprocess.run(['pwsh', '-c', 'Connect-AzAccount; Get-AzVM'], capture_output=True, text=True)
+    if result.returncode == 0:
+        return result.stdout
+    else:
+        return f"Error: {result.stderr}"
+
+if __name__ == '__main__':
+    app.run()
