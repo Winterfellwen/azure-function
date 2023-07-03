d9e6855f4518cd2806f74f2a2137a278035a9b5e
Winterfellwen <38210291+Winterfellwen@users.noreply.github.com>
2023-07-03 15:16:08 +0800

    Update main.py

--- web/web/main.py
+++ web/web/main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import subprocess
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World from Peter Flask"
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+@app.route('/get_vm')
+def get_vm():
+    result = subprocess.run(['pwsh', '-c', 'Connect-AzAccount; Get-AzVM'], capture_output=True, text=True)
+    if result.returncode == 0:
+        return result.stdout
+    else:
+        return f"Error: {result.stderr}"
+
+if __name__ == '__main__':
+    app.run()
