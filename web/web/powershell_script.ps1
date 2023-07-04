559f4d0fc784cf30df2c9e647a41f5d7849dd118
Winterfellwen <38210291+Winterfellwen@users.noreply.github.com>
2023-07-04 15:02:25 +0800

    Update powershell_script.ps1

--- web/web/powershell_script.ps1
+++ web/web/powershell_script.ps1
@@ -3,10 +3,11 @@ $ApplicationId = "a612ea48-5e33-4911-a44b-2a4ee3225fb7"
 $PWord = "NkH8Q~uIKp7mr6jvmGWVvbA4pU42K1wuZ5VO5bmU"
 $SecuredPassword = ConvertTo-SecureString -String $PWord -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApplicationId, $SecuredPassword
-Connect-AzAccount -ServicePrincipal -TenantId $TenantId -Credential $Credential | out-null
+Connect-AzAccount -ServicePrincipal -TenantId $TenantId -Credential $Credential -WarningAction ignore| out-null
 
-$result = get-azvm
-get-azvm
+$result = get-azvm | Out-String
 
-write-host "test" 
+write-host "test: get-azvm" 
 write-host $result
+
+Disconnect-AzAccount| out-null
