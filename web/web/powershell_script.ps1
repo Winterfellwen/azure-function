0179e59c80516bc227aace54528e2893613b2e35
Winterfellwen <38210291+Winterfellwen@users.noreply.github.com>
2023-07-04 16:55:07 +0800

    Update powershell_script.ps1

--- web/web/powershell_script.ps1
+++ web/web/powershell_script.ps1
@@ -1,3 +1,15 @@
+cd /home/azuser/.Azure
+$keyfile = get-content keystore.cache | ConvertFrom-Json
+$keyfile.keyStoreValue.trim('"')
+$keybase =$keyfile.keyStoreKey | ConvertFrom-json
+
+$TenantId = $keybase.tenantId
+$ApplicationId = $keybase.appId
+$PWord = $keyfile.keyStoreValue.trim('"')
+$SecuredPassword = ConvertTo-SecureString -String $PWord -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApplicationId, $SecuredPassword
+Connect-AzAccount -ServicePrincipal -TenantId $TenantId -Credential $Credential -WarningAction ignore| out-null
+
 $result = get-azvm | Out-String
 
 write-host "test: get-azvm" 
