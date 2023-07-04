3a520586160e36d2217d55cfef12e2b8c7c27c75
Winterfellwen <38210291+Winterfellwen@users.noreply.github.com>
2023-07-04 14:24:16 +0800

    Update main.py

--- web/web/main.py
+++ web/web/main.py
@@ -3,9 +3,15 @@ import subprocess
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World from Peter Flask"
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/execute', methods=['POST'])
+def execute():
+    script_path = './powershell_script.ps1'  # 替换为你的 PowerShell 脚本路径
+    result = subprocess.check_output(['powershell', '-File', script_path]).decode('utf-8')
+    return result
 
 if __name__ == '__main__':
     app.run()
