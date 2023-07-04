ed8956bc31573c4240a0562963ede1f8776e54b9
Winterfellwen <38210291+Winterfellwen@users.noreply.github.com>
2023-07-04 14:35:06 +0800

    Update main.py

--- web/web/main.py
+++ web/web/main.py
@@ -10,7 +10,7 @@ def index():
 @app.route('/execute', methods=['POST'])
 def execute():
     script_path = './powershell_script.ps1'  # 替换为你的 PowerShell 脚本路径
-    result = subprocess.check_output(['powershell', '-File', script_path]).decode('utf-8')
+    result = subprocess.check_output(['pwsh', script_path]).decode('utf-8')
     return result
 
 if __name__ == '__main__':
